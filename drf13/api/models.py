f94800f21ba04f700ce14965bf1083d5de389e5f
kamal hasan <hasan.kamaal0072@gmail.com>
2021-03-15 09:14:41 -0800

    api

--- drf13/api/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Student(models.Model):
-    name=models.CharField(max_length=90)
-    email=models.EmailField(max_length=100)
-    address=models.CharField(max_length=190)
